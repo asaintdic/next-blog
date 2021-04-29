079454d00266b30b9c4df34f27cbb1a74ea3ecca
Alain <alains214@gmail.com>
2021-04-28 23:58:50 -0400

    add image folder, profile image, and change

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,4 +1,18 @@
-export default function() {
-    return <h1>First Post</h1>
-    
+import Link from 'next/link'
+import Head from 'next/head'
+
+export default function FirstPost() {
+    return (
+        <>
+        <Head>
+          <title>First Post</title>
+        </Head>
+          <h1>First Post</h1>
+          <h2>
+            <Link href="/">
+              <a>Back to home</a>
+            </Link>
+          </h2>
+        </>
+      )
 }
